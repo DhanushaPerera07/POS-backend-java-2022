17834c8ea6b40284dd1498d3ed7ebea2f6eeb6f4
R.A.D.B.S.PERERA_IT18231656 <it18231656@my.sliit.lk>
2022-07-04 06:55:58 +0530

    Use jsonb and CustomerBO.

--- src/main/java/lk/ijse/dep/api/CustomerServlet.java
+++ src/main/java/lk/ijse/dep/api/CustomerServlet.java
@@ -23,6 +23,16 @@
  */
 package lk.ijse.dep.api;
 
+import lk.ijse.dep.business.BOFactory;
+import lk.ijse.dep.business.BOTypes;
+import lk.ijse.dep.business.custom.CustomerBO;
+import lk.ijse.dep.constant.CommonConstant;
+import lk.ijse.dep.constant.MimeTypes;
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import javax.json.bind.Jsonb;
+import javax.json.bind.JsonbBuilder;
+import javax.json.bind.JsonbException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -30,15 +40,43 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
 
 @WebServlet(urlPatterns = "/api/v1/customers")
 public class CustomerServlet extends HttpServlet {
 
+    Jsonb jsonb = JsonbBuilder.create();
+
+    CustomerBO customerBO = BOFactory.getInstance().getBO(BOTypes.CUSTOMER);
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("Hello World!");
-        resp.setContentType("text/html");
+        /* get the print-writer. */
         PrintWriter out = resp.getWriter();
-        out.println("<h1>Hello Servlet!</h1>");
+
+        /* set respond type. */
+        resp.setContentType(MimeTypes.Application.JSON);
+
+        /* get reference of the basic datasource from the servlet context. */
+        BasicDataSource basicDataSource = (BasicDataSource) getServletContext()
+                .getAttribute(CommonConstant.CONNECTION_POOL_NAME);
+
+        try {
+            customerBO.setConnection(basicDataSource.getConnection());
+        } catch (SQLException e) {
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            e.printStackTrace();
+        }
+
+        try {
+            out.println(jsonb.toJson(customerBO.findAllCustomers()));
+        } catch (JsonbException e) {
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            e.printStackTrace();
+        } catch (SQLException e) {
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            e.printStackTrace();
+        }
+
     }
 }
